7336ee1b2b7fa5b1d3edf4fa2fdee5e1eb4e0831
runfa.luo <runfa.luo@transsion.com>
2023-01-10 18:06:26 +0800

    update

---
+++ checkin.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from __future__ import print_function
+import requests
+import time
+
+def getCookie():
+    print("-------------开始登录-------------------")
+    
+    timestamp = str( int(str(time.time()).split('.')[0]) + 86400 )
+    cookies = {
+        'ip': '720c880ad931cc350c791a2d79680e18',
+        'expire_in': timestamp,
+    }
+    
+    data = {
+        'code': '',
+        'email': '373665997@qq.com',
+        'passwd': 'XXXXXX',
+        'fingerprint': '4fa3d911e01ebb8d175c6ab3ec8f0579',
+    }
+    
+    headers = {
+        'authority': 'neworld.cloud',
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'origin': 'https://neworld.cloud',
+        'referer': 'https://neworld.cloud/auth/login',
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest',
+    }
+    
+    response = requests.post('https://neworld.cloud/auth/login', cookies=cookies, headers=headers, data=data)
+    cookies = requests.utils.dict_from_cookiejar(response.cookies)
+    return response, cookies
+
+
+def checkin(cookies):
+    print("-------------开始签到-------------------")
+    
+    headers = {
+        'authority': 'neworld.cloud',
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'origin': 'https://neworld.cloud',
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest',
+    }
+    
+    response = requests.post('https://neworld.cloud/user/checkin', cookies=cookies, headers=headers)
+    return response
+
+
+def getinform(response):
+    print(eval(response.text)['msg'])
+    
+    print("返回信息:", end="")
+    print( response.url, response.status_code, response.reason, response.encoding, response.apparent_encoding, response.text )
+    print("认证信息:", end="")
+    print( requests.utils.dict_from_cookiejar(response.cookies) )
+
+
+if __name__=='__main__':
+    response, cookies  = getCookie()
+    getinform(response)
+    
+    response = checkin(cookies)
+    getinform(response)
+
+
