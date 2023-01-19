af8bb0b32e50af89a6481ba684d934169d82b053
RunFIFA <373665997@qq.com>
2023-01-19 15:30:25 +0800

    modify at 2023-01-19 15:30:25

---
+++ system/autopush.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+log(){ echo -e "\e[32m$1 \e[0m"; }
+warn(){ echo -e "\e[31m$1 \e[0m"; }
+
+function push()
+{
+  while true
+  do
+    arg=${1:-main}
+    time=`date '+%F %T'`
+    
+    result=`git status 2>&1`
+    if [[ "$result" =~ "Changes" || "$result" =~ "push" ]]
+    then
+      echo "$result"
+    else
+      echo "$result"
+      break
+    fi
+    
+    git add .
+    git commit -m "modify at $time"
+    result=`git push origin -u $arg 2>&1`
+    if [[ $result =~ "fatal" ]]
+    then
+      echo "$result"
+      warn "git push failed, try again..."
+    else
+      echo "$result"
+      break
+    fi
+    
+  done
+}
+
+function pull()
+{
+  while true
+  do
+    result=`git pull 2>&1`
+    if [[ $result =~ "fatal" ]]
+    then
+      echo "$result"
+      warn "git pull failed, try again..."
+    else    
+      echo "$result"
+      break
+    fi
+  done
+}
+
+function autopush()
+{
+  while true
+  do
+    log "------------------start to pull------------------!"
+    pull
+    log "------------------start to push------------------!"
+    push
+    log "------------------start to wait------------------!"
+    date=`date '+%F %T' --date="+6 hour"`
+    echo "waiting for 6 hour ..."
+    echo  $date will sync again
+    sleep 6h
+  done
+}
+
+autopush
